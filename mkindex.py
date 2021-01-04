85bddf62ab05d8572dcd0785483619f1654a9b29
Johann Rudloff <johann@sinyax.net>
2021-01-04 17:54:40 +0100

    Initial commit

---
+++ mkindex.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import json
+from pathlib import Path
+import re
+import traceback
+from typing import NamedTuple
+
+from bs4 import BeautifulSoup
+
+class IndexEntry(NamedTuple):
+    name: str
+    namespace: str
+    package: str
+    target: str
+
+RE_NAMESPACE = re.compile(r"""(?P<ns>([^.]+\.)+)(?P<name>.+)""")
+def split_ns(full_name):
+    match = RE_NAMESPACE.match(full_name)
+    return match.group('ns')[:-1], match.group('name')
+
+def sortkey(x):
+    return (x.name.lower(),
+            x.namespace.lower(),
+            x.package.lower(),
+            x.target
+            )
+
+class IndexBuilder:
+    def __init__(self, root: str):
+        self.root = Path(root)
+        self.entries = []
+
+    def run(self):
+        sources = self.root.glob('**/*.html')
+
+        for source in sources:
+            try:
+                self.scan(source)
+            except Exception as e:
+                print(f"error processing file {source}")
+                traceback.print_exc()
+
+        self.entries.sort(key=sortkey)
+        with open(self.root / 'index.json', 'w') as f:
+            json.dump(self.entries, f)
+
+    def scan(self, source):
+        entries = []
+        relpath = source.relative_to(self.root)
+        package = relpath.parts[0]
+        print(f"scanning {source}")
+
+        with open(source) as f:
+            soup = BeautifulSoup(f, 'html.parser')
+
+        docclass = soup.find('body')['class']
+        if 'namespace' not in docclass:
+            return
+
+        for span in soup.select('dl.decls > dt > span.name'):
+            id = span.parent['id']
+            full_name = span.get_text(strip=True)
+            namespace, name = split_ns(full_name)
+            # print(f"{full_name} -> {namespace} , {name}")
+            # namespace, name = full_name.rsplit('.', 1)
+            entries.append(IndexEntry(
+                name,
+                namespace,
+                package,
+                relpath.as_posix() + '#' + id
+            ))
+        self.entries += entries
+
+if __name__ == '__main__':
+    import sys
+    builder = IndexBuilder(sys.argv[1])
+    builder.run()
