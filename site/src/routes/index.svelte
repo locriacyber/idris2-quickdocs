ac5f91e08278caa448b38d9c5c348aaa8280ac63
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2022-04-22 17:41:31 +0800

    Remake index page in Svelte

---
+++ site/src/routes/index.svelte
@@ -0,0 +1,21 @@
+<style>
+	@import "../style.css";
+</style>
+
+<div class="flex-container">
+  <div id="sidebar">
+    <div class="searchbox-container">
+      <input
+        type="text"
+        class="searchbox"
+        id="i2d_searchbox"
+        placeholder="Type to begin searching"
+      />
+      <span class="key-shortcut">Tab ⇥</span>
+    </div>
+    <ul id="i2d_search_results"></ul>
+  </div>
+  <div id="content">
+    <iframe style="width: 100%; height: 100%;" src="data/home.html"> </iframe>
+  </div>
+</div>
